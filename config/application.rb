52160dab19b06154f8fc4ca4881376b8b5fd78f1
takumi2020 <takumi_h@hamata9.com>
2020-09-13 16:58:32 +0900

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,29 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module SubsctimeApp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+    config.time_zone = 'Tokyo' #Time.zone.nowとかの時に日本時間を使用する
+    config.active_record.default_timezone = :local #DBの読み書きを日本時間でやる
+    config.i18n.default_locale = :ja
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+      
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
