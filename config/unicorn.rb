3b49e60b75415665e3d8c5b05ff0e4656376ee1b
takumi2020 <takumi_h@hamata9.com>
2020-09-14 10:19:03 +0900

    updata unicron.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,4 @@
+#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
 app_path = File.expand_path('../../', __FILE__)
 
 #アプリケーションサーバの性能を決定する
@@ -10,7 +11,7 @@ working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen "#{app_path}/tmp/sockets/unicorn.sock"
+listen 3000
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
