8d21931d1e10a8223e23ad6e32fb6e2df6591c86
takumi2020 <takumi_h@hamata9.com>
2020-09-15 15:00:28 +0900

    swiper実装

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,24 +1,45 @@
-let mySwiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-})
+$(function () {
+  // new Swiper('.swiper-container', {
+  //   // Optional parameters
+  //   direction: 'vertical',
+  //   loop: true,
+  
+  //   // If we need pagination
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //   },
+  
+  //   // Navigation arrows
+  //   navigation: {
+  //     nextEl: '.swiper-button-next',
+  //     prevEl: '.swiper-button-prev',
+  //   },
+  
+  //   // And if we need scrollbar
+  //   scrollbar: {
+  //     el: '.swiper-scrollbar',
+  //   },
+  // })
+  new Swiper('.swiper-container', {
+      navigation: {
+        prevEl: '.swiper-button-prev', //左側にあるこれ「＜」
+        nextEl: '.swiper-button-next', //右側にあるこれ「＞」
+      },
+    loop: true, //画像のループの有無
+      effect: 'fade',
+      
+      speed: 3000, //スライドするスピード
+      pagination: { //ページネーションの種類
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true, //クリック判定の有無
+      },
+      autoplay: { //自動スライド
+        delay: 6000, //3秒ごとに
+        disableOnInteraction: true
+      },
+    });
+});
 
 
 // var swiper = new Swiper('.swiper-container', {
