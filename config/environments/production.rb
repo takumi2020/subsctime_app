131f0e039b5891304507214d0aaca4fed23c4bb3
takumi2020 <takumi_h@hamata9.com>
2020-10-01 21:36:33 +0900

    fix:sbout.scss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
   config.assets.compress = true
-  config.assets.digest = false
+  config.assets.digest = true
+  config.public_file_server.enabled = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
