5293c7914e53e4d09edb6e820d145be159e9200e
takumi2020 <takumi_h@hamata9.com>
2020-10-04 23:11:18 +0900

    テストコード記述2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 describe User do
   describe '#create' do
-    it "is invalid without a name" do
+    it "nameがない場合は登録できないこと" do
       user = User.new(name: "", email: "kkk@gmail.com", password: "0000aaaa", password_confirmation: "0000aaaa")
       user.valid?
       expect(user.errors[:name]).to include("を入力してください")
     end
+
+    it "emailがない場合は登録できないこと" do
+      user = User.new(name: "test", email: "", password: "0000aaaa", password_confirmation: "0000aaaa")
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
+
+    it "passwordがない場合は登録できないこと" do
+      user = User.new(name: "test", email: "kkk@gmail.com", password: "", password_confirmation: "0000aaaa")
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
+
   end
 end
