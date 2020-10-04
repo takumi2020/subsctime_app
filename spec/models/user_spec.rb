cc0e57254166c36b45b7b198c54c4eb9103a04d3
takumi2020 <takumi_h@hamata9.com>
2020-10-04 23:41:34 +0900

    user_apec修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 describe User do
   describe '#create' do
+    it "name, email, password, password_confirmationが入力されていたら登録できること" do
+      user = build(:user)
+      user.valid?
+      expect(user).to be_valid
+    end
+
     it "nameがない場合は登録できないこと" do
       user = build(:user, name: "")
       user.valid?
@@ -8,43 +14,52 @@ describe User do
     end
 
     it "emailがない場合は登録できないこと" do
-      user = User.new(name: "test", email: "", password: "0000aaaa", password_confirmation: "0000aaaa")
+      user = build(:user, email: "")
       user.valid?
       expect(user.errors[:email]).to include("を入力してください")
     end
 
-    it "不正なアドレス場合は登録できないこと" do
-      user = User.new(name: "test", email: "kkkgmail.com", password: "0000aaaa", password_confirmation: "0000aaaa")
+    it "@がないアドレス場合は登録できないこと" do
+      user = build(:user, email: "kkkgmail.com")
       user.valid?
       expect(user.errors[:email]).to include("は不正な値です")
     end
 
-    it "不正なアドレス場合は登録できないこと" do
-      user = User.new(name: "test", email: "kkk@gmailcom", password: "0000aaaa", password_confirmation: "0000aaaa")
+    it "不正なアドレスgmailcom場合は登録できないこと" do
+      user = build(:user, email: "kkk@gmailcom")
       user.valid?
       expect(user.errors[:email]).to include("は不正な値です")
     end
 
-    it "不正なアドレス場合は登録できないこと" do
-      user = User.new(name: "test", email: "@gmail.com", password: "0000aaaa", password_confirmation: "0000aaaa")
+    it "アドレスの＠より前がない場合は登録できないこと" do
+      user = build(:user, email: "@gmail.com")
       user.valid?
       expect(user.errors[:email]).to include("は不正な値です")
+    end
+
+    it "メールアドレスが同じ場合登録できないこと" do
+      #はじめにユーザーを登録
+      user = create(:user)
+      #先に登録したユーザーと同じemailの値を持つユーザーのインスタンスを作成
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("はすでに存在します")
     end
 
     it "passwordがない場合は登録できないこと" do
-      user = User.new(name: "test", email: "kkk@gmail.com", password: "", password_confirmation: "0000aaaa")
+      user = build(:user, password: "")
       user.valid?
       expect(user.errors[:password]).to include("を入力してください")
     end
 
     it "password確認がない場合は登録できないこと" do
-      user = User.new(name: "test", email: "kkk@gmail.com", password: "0000aaaa", password_confirmation: "")
+      user = build(:user, password_confirmation: "")
       user.valid?
       expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
     end
 
     it "password一致しない場合は登録できないこと" do
-      user = User.new(name: "test", email: "kkk@gmail.com", password: "0000aaaa", password_confirmation: "1111aaaa")
+      user = build(:user, password_confirmation: "1111aaaa")
       user.valid?
       expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
     end
