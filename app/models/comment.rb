8dd82f1634d95341ee13d75136221fb9348dce2d
takumi2020 <takumi_h@hamata9.com>
2020-10-05 22:16:44 +0900

    コメントバリデーション追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :product  # tweetsテーブルとのアソシエーション
   belongs_to :user  # usersテーブルとのアソシエーション
+
+
+  validates :text, presence: true, length: {maximum: 20}
+
 end
