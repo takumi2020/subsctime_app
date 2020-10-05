dac488ee1ebf01c6237aa52b63e614990986c8ed
takumi2020 <takumi_h@hamata9.com>
2020-10-05 21:01:41 +0900

    テスト追加

--- spec/requests/devise_users_spec.rb
+++ spec/requests/devise_users_spec.rb
@@ -43,10 +43,28 @@ RSpec.describe "UserAuthentications", type: :request do
         end.to_not change(User, :count)
       end
 
-      # it 'エラーが表示されること' do
-      #   post user_registration_path, params: { user: invalid_user_params }
-      #   expect(response.body).to include 'prohibited this user from being saved'
-      # end
     end
   end
+
+  # describe 'GET #edit' do
+  #   subject { get edit_user_registration_path }
+  #   context 'ログインしている場合' do
+  #     before do
+  #       sign_in user
+  #     end
+  #     it 'リクエストが成功すること' do
+  #       is_expected.to eq 200
+  #     end
+  #   end
+  # end
+
+  # describe 'GET #show' do
+  #   context 'ユーザーが存在しない場合' do
+  #     it 'エラーが発生すること' do
+  #       user_id = user.id
+  #       user.destroy
+  #       expect{ get "/users" }.to raise_error ActiveRecord::RecordNotFound
+  #     end
+  #   end
+  # end
 end
