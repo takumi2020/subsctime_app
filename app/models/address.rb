59289d7907ec598e195bcd143ccaf33fa2fd4b94
takumi2020 <takumi_h@hamata9.com>
2020-11-07 22:25:00 +0900

    コード削除

--- app/models/address.rb
+++ app/models/address.rb
@@ -7,7 +7,7 @@ class Address < ApplicationRecord
 
   validates :name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
   validates :name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :postal_code, presence: true, format: { with: /\A\d{7}\z/ }  #ハイフンなし7桁
+  validates :postal_code, presence: true, format: { with: /\A\d{7}\z/ }
   validates :prefecture_id, :city, :house_number, :tell, presence: true
   validates :building_name, presence: false
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -40,10 +40,8 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
-      user.name = "ゲストユーザー"
-    # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+      user.name = "GuestUser"
     end
   end
-
   
 end
