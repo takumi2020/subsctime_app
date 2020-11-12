e97077321e8fb073992207cbfb6c163c9b05ab1f
takumi2020 <takumi_h@hamata9.com>
2020-11-12 21:57:23 +0900

    fixaa

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable,
           :omniauthable, omniauth_providers: %i(google)
 
-
-          
   validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: true }, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/i }
   validates :password, presence: true, length: { minimum: 7 }, format: { with: /(?=.*\d+.*)(?=.*[a-zA-Z]+.*)./ }
