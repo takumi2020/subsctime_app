b63e424039ed21a24b9bb2c7b5ce53a629ed147c
takumi2020 <takumi_h@hamata9.com>
2020-11-27 23:00:15 +0900

    ap

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,6 @@
-# ../が一つ増えている
 app_path = File.expand_path('../../../', __FILE__)
 
 worker_processes 1
-# currentを指定
 working_directory "#{app_path}/current"
 
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
